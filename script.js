907c5c54ecb4029cb9a20842fa60393dca9cfc71
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-04-24 20:39:07 +0200

    Update

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
   document.querySelector('.message').textContent = 'Start Guessing... ';
-  document.querySelector('.score').textContent = score;
+  document.querySelector('.score').textContent = 20;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
